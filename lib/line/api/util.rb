245ff27437e2f5e598fb87ecf336a8f564f17c7d
Ryuji <promi0904@gmail.com>
2016-05-15 14:31:30 +0900

    LINE Channel App Web用の認証の複合処理をline api gemに移譲

--- lib/line/api/util.rb
+++ lib/line/api/util.rb
@@ -7,10 +7,10 @@ module Line
 
       class << self
         # for Channel Web Application
-        def retrieve_access_token_from_encrypted_string(encrypted_string, channel_secret)
+        def retrieve_access_token_from_encrypted_string(encrypted_string)
           cipher = OpenSSL::Cipher::Cipher.new('AES-128-ECB')
           cipher.decrypt
-          cipher.key = [channel_secret].pack('H*')
+          cipher.key = [Line::Api::Configuration.channel_secret].pack('H*')
           decoded = Base64.decode64(encrypted_string)
           decrypted = cipher.update(decoded) + cipher.final
           mid, access_token, expire, refresh_token = decrypted.split('.')
